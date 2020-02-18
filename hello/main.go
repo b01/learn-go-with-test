909c84f81af58d959ebfb988bc7c87619001599b
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2020-02-17 22:13:17 -0500

    Refactoed hello package.

--- hello/main.go
+++ hello/main.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
-
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, %v", name)
+func Hello(name, lang string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greeting(lang) + name
 }
 
+func greeting(lang string) string {
+	prefix := "Hello, "
+
+	switch lang {
+	case "es":
+		prefix = "Hola, "
+	case "fr":
+		prefix = "Bonjour, "
+	}
+
+	return prefix
+}
 func main () {
-	Hello("World")
+	Hello("World", "en")
 }
--- hello/maint_test.go
+++ hello/maint_test.go
@@ -5,10 +5,29 @@ import(
 )
 
 func TestHello(t *testing.T) {
-	want := "Hello, Chris"
-	got := Hello("Chris")
 
-	if want != got {
-		t.Errorf("wanted %q got %q", want, got)
+	assertHelloOutput := func(t *testing.T, w, g string) {
+		t.Helper()
+		if w != g {
+			t.Errorf("wanted %q got %q", w, g)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		want := "Hello, Chris"
+		got := Hello("Chris", "")
+		assertHelloOutput(t, want, got)
+	})
+
+	t.Run("empty string defaults to World", func(t *testing.T) {
+		want := "Hello, World"
+		got := Hello("", "")
+		assertHelloOutput(t, want, got)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		want := "Hola, Elodie"
+		got := Hello("Elodie", "es")
+		assertHelloOutput(t, want, got)
+	})
 }
