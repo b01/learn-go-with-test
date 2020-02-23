e36731daf2eda1b356ef5017f339b70fea40b0ee
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2020-02-23 08:48:36 -0500

    Completed Arrays and Slices 2.

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -9,3 +9,13 @@ func Sum(n []int) int {
 
 	return s
 }
+
+func SumAll(numbers ...[]int) ([]int) {
+	var sums []int
+
+	for _, v := range numbers {
+		sums = append(sums, Sum(v))
+	}
+
+	return sums
+}
--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,10 +1,11 @@
 package main
 
 import(
+	"reflect"
 	"testing"
 )
 
-func TestSum(t *testing.T)  {
+func TestSum(t *testing.T) {
 	t.Run("test sum with array", func(t *testing.T) {
 		n := []int{1, 2, 3, 4, 5}
 		g := Sum(n)
@@ -14,4 +15,20 @@ func TestSum(t *testing.T)  {
 			t.Errorf("expecting %d got %d given, %v", e, g, n)
 		}
 	})
+}
+func TestSumAll(t *testing.T) {
+	t.Run("Sum all", func(t *testing.T) {
+		g := SumAll([]int{1, 3}, []int{2, 3})
+		e := []int{4, 5}
+
+		if !reflect.DeepEqual(g, e) {
+			t.Errorf("expecting %v got %v", e, g)
+		}
+	})
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAll([]int{1, 2, 3}, []int{4, 6, 2, 3})
+	}
 }
