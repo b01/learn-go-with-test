2d30902ce168293dba77164b044f38a77ca4b735
Khalifah K. Shabazz <khalifahshabazz@quickenloans.com>
2020-03-06 13:27:19 -0500

    Completed Mocking 3.

--- mocking/main.go
+++ mocking/main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 func CountDown(w io.Writer)  {
 	for i := 3; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(w, i)
 	}
+
+	time.Sleep(1 * time.Second)
 	fmt.Fprint(w, "Go!")
 }
 
